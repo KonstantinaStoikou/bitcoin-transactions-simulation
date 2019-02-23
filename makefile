eee4242b942ab053508386850cd8ae75ca9b037d
Konna <tinasto97@gmail.com>
2019-02-24 01:41:23 +0200

    Add test main

--- makefile
+++ makefile
@@ -1,9 +1,16 @@
-OUT  	= bitcoin
+OUT  	= bitcoin demo
 CC		= gcc
 FLAGS   = -g -Wall
 
+all: $(OUT)
+
+# the app executable
 bitcoin: src/main.c
 	$(CC) $(FLAGS) src/main.c src/read_functions.c -o bitcoin
+
+# a main function for testing new features or changes
+demo: test/main.c
+	$(CC) $(FLAGS) test/main.c src/read_functions.c -o demo
 
 clean:
 	rm -f $(OUT)
--- test/main.c
+++ test/main.c
@@ -2,19 +2,21 @@
 #include "../include/read_functions.h"
 
 int main(int argc, char const *argv[]) {
+    printf("THIS IS A TEST MAIN!");
+
     char *bitcoin_balances_file, *transaction_file;
     int bitcoin_value, sender_hashtable_num_of_entries,
         receiver_hashtable_num_of_entries, bucket_size;
     read_arguments(argc, argv, &bitcoin_balances_file, &transaction_file,
                    &bitcoin_value, &sender_hashtable_num_of_entries,
                    &receiver_hashtable_num_of_entries, &bucket_size);
-    printf("bitcoin_balances_file:             %s\n", bitcoin_balances_file);
-    printf("transaction_file:                  %s\n", transaction_file);
-    printf("bitcoin_value:                     %d\n", bitcoin_value);
-    printf("sender_hashtable_num_of_entries:   %d\n",
+    printf("bitcoin_balances_file: %s\n", bitcoin_balances_file);
+    printf("transaction_file: %s\n", transaction_file);
+    printf("bitcoin_value: %d\n", bitcoin_value);
+    printf("sender_hashtable_num_of_entries: %d\n",
            sender_hashtable_num_of_entries);
     printf("receiver_hashtable_num_of_entries: %d\n",
            receiver_hashtable_num_of_entries);
-    printf("bucket_size:                       %d\n", bucket_size);
+    printf("bucket_size: %d\n", bucket_size);
     return 0;
 }
