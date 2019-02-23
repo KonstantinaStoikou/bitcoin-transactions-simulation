26c8af101989e6fbe05caa69916cfd2a9d00b5cc
Konna <tinasto97@gmail.com>
2019-02-23 19:34:25 +0200

    Read command line arguments

--- src/main.c
+++ src/main.c
@@ -8,5 +8,13 @@ int main(int argc, char const *argv[]) {
     read_arguments(argc, argv, &bitcoin_balances_file, &transaction_file,
                    &bitcoin_value, &sender_hashtable_num_of_entries,
                    &receiver_hashtable_num_of_entries, &bucket_size);
+    printf("bitcoin_balances_file: %s\n", bitcoin_balances_file);
+    printf("transaction_file: %s\n", transaction_file);
+    printf("bitcoin_value: %d\n", bitcoin_value);
+    printf("sender_hashtable_num_of_entries: %d\n",
+           sender_hashtable_num_of_entries);
+    printf("receiver_hashtable_num_of_entries: %d\n",
+           receiver_hashtable_num_of_entries);
+    printf("bucket_size: %d\n", bucket_size);
     return 0;
 }
